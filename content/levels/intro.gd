d1a8a024f963cda299c7a483dbba697fb75a0686
aidanstout <stouta@g.cofc.edu>
2024-12-04 17:53:34 -0500

    Intro fix
    Added timing, sound, and connection to the playable scene(one_world.tscn)

--- content/levels/intro.gd
+++ content/levels/intro.gd
@@ -1,10 +1,11 @@
 extends Node
 @export var text : RichTextLabel
+@export var main_scene:PackedScene
 func _ready() -> void:
 	text.visible_ratio = 0
 
 func _process(delta: float) -> void:
-	text.visible_ratio += 0.5 * delta
-	if text.visible_ratio == 1:
-		pass
-		#this should switch to main
+	text.visible_ratio += 0.1 * delta #builds the text out letter by letter
+	
+	if text.visible_ratio == 1: #switch scene after all the text is visible
+		get_tree().change_scene_to_packed(main_scene)
